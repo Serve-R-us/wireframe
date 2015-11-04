d93554988090953ecbf8fbcbbb7e9d78c45ef362
James <jamesrwads@gmail.com>
2015-11-04 13:33:15 -0500

    Added logging in and logging out

---
+++ process.php
@@ -0,0 +1,28 @@
+
+<?php
+    require_once('/config/connection.php');
+?>
+    <?php include('templates/header.php'); ?>
+    <?php include('templates/nav.php'); ?>
+    <div class="container">
+        
+        <?php
+            
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            
+            echo "{$username} : {$password} <br />";
+            
+                $member_query = "SELECT * FROM users WHERE user = '$_POST[username]' AND password = SHA1('$_POST[password]')";
+                $member_result = mysqli_query($dbc, $member_query);
+                
+                if ($member_result) {
+                    echo "This ran?";
+                    $_SESSION['username'] = $username;
+                    header('Location: index.php');
+                }
+        ?>
+        
+    </div>
+        
+    <?php include('templates/footer.php'); ?>
