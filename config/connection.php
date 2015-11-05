95df016e0c6b89160b8437ce5ca17398505e2832
DeanWay <deanway93@gmail.com>
2015-11-05 10:54:13 -0500

    added sql query in connection.php to use serverus database in subsequent queries

--- config/connection.php
+++ config/connection.php
@@ -3,5 +3,7 @@
     session_start();
 
     $dbc = mysqli_connect('localhost', 'root', 'PositiveFish', 'serverus') OR die('Could not connect because: ' . mysqli_connect_error());
+    $q_string = "USE serverus;";
+    mysqli_query($dbc, $q_string);
 
 ?>
