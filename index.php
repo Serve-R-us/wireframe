923dcc2ac9920904fa39b57ffbaf82fb5662f628
Sunjot Singh <sunjot.10@gmail.com>
2015-11-08 19:34:13 -0500

    updated index

--- index.php
+++ index.php
@@ -70,26 +70,6 @@
         </div>
     </div>
       
-      
-    <!-- Content section 
-    <div class="row marketing">
-      <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-      </div>
-
-      <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-      </div>
-    </div> -->
-      
   </div> <!-- /container -->
 
 <?php include('templates/footer.php') ?>
