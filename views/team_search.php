0059d7ed0ad59e0b8aabe3614ae94cc582e3f3d7
irenaeus <irenaeus@mail.uoguelph.ca>
2015-11-10 04:49:21 -0500

    Added 404 Page and Team Page

---
+++ views/team_search.php
@@ -0,0 +1,90 @@
+<!-- Content -->
+<div class="container">
+	<div>
+		<h2>Team Search</h2>
+		<hr>
+		<div class="panel panel-default">
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search Teams">
+				</div>
+				<button type="submit" class="btn btn-default">Search</button>
+			</form>
+
+			<!--Table-->
+			<table class="table">
+				<thread>
+					<tr>
+						<th>Team #</th>
+						<th>Team Name</th>
+						<th>Size</th>
+						<th>Profile</th>
+					</tr>
+				</thread>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mealworm Exchange</td>
+						<td>3/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Nambia4Lyfe</td>
+						<td>1/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Serve R Us</td>
+						<td>3/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">4</th>
+						<td>Admin Team</td>
+						<td>2/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">5</th>
+						<td>Guelph Coding Community</td>
+						<td>2/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">6</th>
+						<td>Irenaeus' Team</td>
+						<td>1/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">7</th>
+						<td>Follow Dean's Way</td>
+						<td>3/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">8</th>
+						<td>Matt Beckons</td>
+						<td>1/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">9</th>
+						<td>Wad's #Worth</td>
+						<td>2/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+					<tr>
+						<th scope="row">10</th>
+						<td>It's Always Sunny</td>
+						<td>2/4</td>
+						<td><a href="?page=team_profile">View Team</a></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<!-- /Container-->
